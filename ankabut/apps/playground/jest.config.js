cac12872d6569ea3999f8b0a2fd60294320be85b
Sahandevs <sahandevs@gmail.com>
2020-01-01 23:13:23 +0330

    create playground project

---
+++ ankabut/apps/playground/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'playground',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/playground'
+};
